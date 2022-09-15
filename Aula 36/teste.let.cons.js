dfd35d79e199a7f8eb6b1e378c38dbf64dfdbddc
Dayane Pereira <dayanepereiraliberato@gmail.com>
2022-09-15 15:01:29 -0300

    commit

---
+++ Aula 36/teste.let.cons.js
@@ -0,0 +1,14 @@
+let nome = "Dayane ";
+let idade = 28;
+console.log (nome);
+console.log (idade);
+
+
+function escreveNome() {
+    let nome = "Pereira"
+    console.log (nome);
+    console.log (idade);
+
+}
+
+escreveNome();
